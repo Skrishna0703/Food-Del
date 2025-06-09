45e886f47bdba7823c512170d2927cbb83de1c8c
Shrikrishna <shrikrishnasutar0703@gmail.com>
2025-06-09 07:44:15 +0530

    remove

---
+++ backend/server.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import cors from 'cors';
+import  {connectDB}  from './config/db.js';
+import foodRouter from './routes/foodRoutes.js';
+
+
+//app config
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+//middleware
+app.use(cors());    
+app.use(express.json());
+//routes
+app.get('/', (req, res) => {
+    res.send('Welcome to the backend server!');
+}
+);
+ 
+//import db connection
+
+
+//connect to database
+connectDB();
+
+//api endpoints
+app.use('/api/foods', foodRouter);
+app.use("/images", express.static("uploads"));
+
+//start server
+app.listen(PORT, () => {
+    console.log(`Server is running on port http://localhost:${PORT}`);
+});
+//mongodb+srv://tomatofood:<db_password>@tomatocluster.dsgwbdo.mongodb.net/?
