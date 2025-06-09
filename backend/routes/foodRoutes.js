c66bb746cf8b309fbfa51c1d066e21c7bddf8b6b
Shrikrishna <shrikrishnasutar0703@gmail.com>
2025-06-09 08:07:27 +0530

    update

--- backend/routes/foodRoutes.js
+++ backend/routes/foodRoutes.js
@@ -1,22 +1,20 @@
 import express from 'express';
-import { addFood } from '../controllers/foodControllers.js';
 import multer from 'multer';
-import { listFood,removeFood } from '../controllers/foodControllers.js'; 
-
+import { addFood, listFood, removeFood } from '../controllers/foodControllers.js';
 
 const foodRouter = express.Router();
 
 const storage = multer.diskStorage({
-    destination:"uploads",
-    filename: (req, file, cb) => {
-      return  cb(null, `${Date.now()}-${file.originalname}`);
-    }
+  destination: "uploads",
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}-${file.originalname}`);
+  }
+});
 
-})
 const upload = multer({ storage: storage });
-foodRouter.post("/add", upload.single("image"), addFood);
-foodRouter.get("/list",listFood);
-foodRouter.post("/remove", removeFood);
 
+foodRouter.post("/add", upload.single("image"), addFood);
+foodRouter.get("/list", listFood);
+foodRouter.post("/remove", removeFood); // ✅ Fixed route
 
 export default foodRouter;
