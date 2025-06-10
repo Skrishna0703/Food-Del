156d7143b08c99d5f5be7e05dde97d5c6d5540be
Shrikrishna <shrikrishnasutar0703@gmail.com>
2025-06-10 11:12:01 +0530

    Add tab

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import Sidebar from './components/Sidebar/Sidebar'
+import { Route, Routes } from 'react-router-dom'
+import Add from './pages/add/Add'
+import List from './pages/list/List'
+import Order from './pages/order/Order'
 
 export const App = () => {
   return (
@@ -8,6 +12,13 @@ export const App = () => {
       <Navbar/><hr/>
       <div className="app-content">
         <Sidebar/>
+        <Routes>
+          <Route path="/add" element={<Add/>} />
+          <Route path="/list" element={<List/>} />
+          <Route path="/order" element={<Order/>} />
+
+         
+        </Routes>
       </div>
 
     </div>
---
+++ admin/src/pages/List/List.jsx
@@ -0,0 +1,10 @@
+import { i } from 'framer-motion/client'
+import React from 'react'
+import './List.css'
+const List = () => {
+  return (
+    <div>List</div>
+  )
+}
+
+export default List
