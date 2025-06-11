24125c9851637da755f66b32ead3b42b5e51dcae
Shrikrishna <shrikrishnasutar0703@gmail.com>
2025-06-11 08:13:39 +0530

    List page

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -5,10 +5,13 @@ import { Route, Routes } from 'react-router-dom'
 import Add from './pages/add/Add'
 import List from './pages/list/List'
 import Order from './pages/order/Order'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export const App = () => {
   return (
     <div>
+      <ToastContainer/>
       <Navbar/><hr/>
       <div className="app-content">
         <Sidebar/>
