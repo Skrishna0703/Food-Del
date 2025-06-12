2274ee2be204a16d480fb6460ee808772e7fcc08
Shrikrishna <shrikrishnasutar0703@gmail.com>
2025-06-12 08:38:09 +0530

    update

--- frontend/src/Componets/LoginPopup/LoginPopup.jsx
+++ frontend/src/Componets/LoginPopup/LoginPopup.jsx
@@ -1,13 +1,34 @@
 import React, { useState } from 'react';
 import './LoginPopup.css';
 import { assets } from '../../assets/assets';
+import { useEffect } from 'react';
+import { useContext } from 'react';
+import { StoreContext } from '../../Context/StoreContext';
 
 const LoginPopup = ({ setShowLogin }) => {
-  const [current, setCurrentState] = useState("Sign Up");
+  const {url}=useContext(StoreContext);
+  const [current, setCurrentState] = useState("Login");
+  const [data, setData] = useState({
+    name: "",   
+    email: "",
+    password: ""
+
+  });
+  const onChangehandler =(event)=>{
+    const name= event.target.name;
+    const value= event.target.value;
+    setData({...data, [name]: value});
+  }
+  
+const onLogin = async (event) => {
+  event.preventDefault();
+
+
+  }
 
   return (
     <div className='login-popup'>
-      <form className="login-popup-container">
+      <form onSubmit={onLogin} className="login-popup-container">
         <div className="login-popup-title">
           <h2>{current}</h2>
           <img
@@ -19,10 +40,10 @@ const LoginPopup = ({ setShowLogin }) => {
 
         <div className="login-popup-inputs">
           {current === "Login" ? null : (
-            <input type="text" placeholder="Enter your name" required />
+            <input type="text" name='name' onChange={onChangehandler} value={data.name} placeholder="Enter your name" required />
           )}
-          <input type="email" placeholder="Enter your email" required />
-          <input type="password" placeholder="Enter your password" required />
+          <input type="email" name='email' onChange={onChangehandler} value={data.email} placeholder="Enter your email" required />
+          <input type="password" name='password' onChange={onChangehandler} value={data.password} placeholder="Enter your password" required />
         </div>
 
         <button type="submit">
