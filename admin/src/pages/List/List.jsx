220bbcd2bc6d50076da9d21200bcb4a2d249b503
Shrikrishna <shrikrishnasutar0703@gmail.com>
2025-06-12 07:06:02 +0530

    fix the db errors

--- admin/src/pages/List/List.jsx
+++ admin/src/pages/List/List.jsx
@@ -4,14 +4,14 @@ import './List.css';
 import axios from 'axios';
 import { toast } from 'react-toastify';
 
-const List = () => {
-  const url = "http://localhost:4000";
+const List = ({url}) => {
+  
   const [list, setList] = useState([]);
 
   const fetchList = async () => {
     try {
       const response = await axios.get(`${url}/api/foods/list`);
-      console.log(response);
+      
       if (response.data.success) {
         setList(response.data.foods);
       } else {
@@ -22,6 +22,17 @@ const List = () => {
       toast.error("Failed to fetch food list.");
     }
   };
+
+const removeFood = async (foodId) => {
+  const response = await axios.post(`${url}/api/foods/remove`,{id:foodId});
+  await fetchList();
+  if (response.data.success) {
+    toast.success("Food removed successfully");
+  } else {
+    toast.error(response.data.message);
+  }
+}
+
 
   // Fetch once when component mounts
   useEffect(() => {
@@ -39,7 +50,18 @@ const List = () => {
         <b>Price</b>
         <b>Action</b>
         </div>
-
+        {list.map((item,index)=>{
+          return(
+            <div className= "list-table-format" key={index}>
+               <img src={`${url}/images/`+item.image} alt="" />
+               <p>{item.name}</p>
+               <p>{item.category}</p>
+               <p>{item.price}</p>
+               <p onClick={()=>removeFood(item._id)} className='cursor'>X</p>
+            </div>
+          )
+        }
+        )}
       </div>
     
     </div>
