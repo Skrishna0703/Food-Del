65700ce18d62394c30e9cff191a9a84ff56d96b0
Shrikrishna <shrikrishnasutar0703@gmail.com>
2025-06-16 07:11:02 +0530

    update

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -9,7 +9,7 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 export const App = () => {
-  const url = "http://localhost:4000";
+  const url = "https://tomato-backend-8sua.onrender.com";
   return (
     <div>
       <ToastContainer/>
