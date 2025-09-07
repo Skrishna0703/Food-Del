defcc564bb8b14b10213554d283ed1d452582561
Shrikrishna <shrikrishnasutar0703@gmail.com>
2025-09-07 18:28:10 +0530

    update

--- frontend/src/Componets/Navbar/Navbar.jsx
+++ frontend/src/Componets/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import { useContext, useState } from 'react';
 import './Navbar.css';
 import { assets } from '../../assets/assets.js';
 import { Link, useNavigate } from 'react-router-dom';
